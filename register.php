f9de411ffaf82a48a99321462b1009138e5004ce
C.S. Taylor, Jr <steviesama@gmail.com>
2016-05-09 13:53:36 -0500

    Testing changes to register.php for command line git commit for the first time.

--- register.php
+++ register.php
@@ -1,10 +1,9 @@
 <?php
     session_start();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
-	<title>Trend Logger</title>
+	<title>Idea</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
     <meta name="description" content="WordSpace registration page">
